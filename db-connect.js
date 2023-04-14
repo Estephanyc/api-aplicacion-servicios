ca81712928a11aec4eb725454c0767c9e8910ff8
Estephany <maitilydevidasi@gmail.com>
2023-04-14 12:09:54 -0400

    validate suer

--- db-connect.js
+++ db-connect.js
@@ -2,7 +2,20 @@ var Connection = require("tedious").Connection;
 var Request = require("tedious").Request;
 const config = require("./config.json");
 
-const connection = new Connection(config);
+let connection;
+const getConnection = () => {
+  connection = new Connection(config);
+
+  console.log("llamada de nuevo");
+  if (connection) {
+    console.log("ya estaba abierta");
+    connection = connection;
+  } else {
+    console.log("se abre una nueva");
+
+    connection = new Connection(config);
+  }
+};
 
 function executeStatement(query) {
   return new Promise((resolve, reject) => {
@@ -13,20 +26,18 @@ function executeStatement(query) {
       connection.close();
     });
 
-    let result = [];
+    let rowsData = [];
 
     request.on("row", (columns) => {
+      const columnData = {};
       columns.forEach((column) => {
-        const columnData = {
-          name: column.metadata.colName,
-          value: column.value,
-        };
-        result.push(columnData);
+        columnData[column.metadata.colName] = column.value;
       });
+      rowsData.push(columnData);
     });
 
     request.on("doneInProc", (rowCount, more) => {
-      resolve(result);
+      resolve(rowsData);
     });
     connection.execSql(request);
   });
@@ -53,13 +64,31 @@ function getApps() {
 function validateUser(user, password) {
   return new Promise((resolve, reject) => {
     const query = `select * from USUARIOS where nombre_usuario = '${user}' and clave = '${password}'`;
+    console.log("Antes de connection");
+    getConnection();
     connection.connect((err) => {
+      console.log("se ejecuto validateUser query");
+
       if (err) {
+        console.log(err);
         reject(err);
       }
       const response = executeStatement(query)
         .then((res) => {
-          resolve(res);
+          console.log("se ejecuto validateUser query");
+          let responseData;
+          if (res.length > 0) {
+            responseData = {
+              status: true,
+              data: res[0],
+            };
+          } else {
+            responseData = {
+              status: false,
+              data: res,
+            };
+          }
+          resolve(responseData);
         })
         .catch((err) => {
           console.log(err);
--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const database = require("./db-connect");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+
 app.use(express.json());
+
+app.use(cors());
+
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
@@ -22,11 +36,14 @@ app.get("/apps", (req, res) => {
     .catch((err) => {});
 });
 
-app.get("/validateUser", (req, res) => {
+app.post("/validateUser", (req, res) => {
   let body = req.body;
+  console.log("se llamo a validateUser ");
   database
-    .validateUser(bodyreq.user, body.clave)
+    .validateUser(body.usuario, body.password)
     .then((data) => {
+      console.log("se recibidio respuesta de validateUser ");
+
       res.json({ message: "Validate user", data: data });
     })
     .catch((err) => {});
